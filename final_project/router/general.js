319a1107759640cb5eafe15f2ffbd92d564acc50
venkatesh-reddi <venkateshreddi@outlook.com>
2024-06-08 23:38:54 -0400

    Added Async-Await functions for doing the same functionality with Asynchronously.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,11 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+
+const getAllBooks = () => {
+    return books;
+  };
+
 const doesExist = (username)=>{
     let userswithsamename = users.filter((user)=>{
       return user.username === username
@@ -31,39 +36,45 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books, null, 4));
+public_users.get('/', async function (req, res) {
+  const allbooks = await getAllBooks();
+  return res.send(JSON.stringify(allbooks, null, 4));
   
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  return res.send(books[isbn]);
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const targetISBN = parseInt(req.params.isbn);
+    const targetBook = await books[targetISBN];
+    if (!targetBook) {
+      return res.status(404).json({ message: "ISBN not found." });
+    } else {
+      return res.status(200).json(targetBook);
+    }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    const foundBooks = Object.values(books).filter((book) => book.author === author);
-  
-    if (foundBooks.length > 0) {
-      return res.send(foundBooks);
-    } else {
-      return res.status(404).json({ message: "No books found by this author" });
-    }
+public_users.get('/author/:author', async function (req, res) {
+    const matchingBooks = Object.values(await books).filter(
+        (book) => book.author.toLowerCase() === req.params.author.toLowerCase()
+      );
+      if (matchingBooks.length > 0) {
+        return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+      } else {
+        return res.status(404).json({ message: "No books by that author." });
+      }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    const foundBooks = Object.values(books).filter((book) => book.title === title);
-  
-    if (foundBooks.length > 0) {
-      return res.send(foundBooks);
-    } else {
-      return res.status(404).json({ message: "No books found with this title" });
-    }
+public_users.get('/title/:title', async function (req, res) {
+    const matchingTitle = Object.values(await books).filter(
+        (book) => book.title.toLowerCase() === req.params.title.toLowerCase()
+      )[0];
+      if (matchingTitle) {
+        return res.status(200).json(matchingTitle);
+      } else {
+        return res.status(404).json({ message: "Title not found." });
+      }
 });
 
 //  Get book review
