ac8f0dfdfe247e9870c6ebb37b38b439231d411a
venkatesh-reddi <venkateshreddi@outlook.com>
2024-06-08 22:25:11 -0400

    Added code for getting the book details based on the title.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,8 +36,14 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const foundBooks = Object.values(books).filter((book) => book.title === title);
+  
+    if (foundBooks.length > 0) {
+      return res.send(foundBooks);
+    } else {
+      return res.status(404).json({ message: "No books found with this title" });
+    }
 });
 
 //  Get book review
